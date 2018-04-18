395e3833d3c2bc1871098f926964208e1834de1e
Maciej Dudek <maciejdudek96@wp.pl>
2018-04-18 09:49:01 +0200

    F1
    Implementation of the findPostOrder function. The program works correctly.

--- [F1] Pre, in and post/Pre, in and post.cpp
+++ [F1] Pre, in and post/Pre, in and post.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void findPostOrder(int left, int right);
+int index;
+
+void findPostOrder(int left, int right, string pree, string inn); //recurcive function
 
 int main() {
 
@@ -10,17 +12,34 @@ int main() {
 
     for(int i = 0; i < cases; ++i) {
 
-        int length, index = 0;
+        int length;
+        index = 0;
         string pre, in;
+
         cin >> length >> pre >> in;
 
-        findPostOrder(0, length-1);
+        findPostOrder(0, length-1, pre, in);
+
         cout << endl;
     }
 
     return 0;
 }
 
-void findPostOrder(int left, int right) {
+void findPostOrder(int left, int right, string pree, string inn) {
+
+    if(left <= right) {
+        int j;
+        for(j = left; j <= right; j++) {
+            if(inn[j] == pree[index]) break;
+        }
+
+        if(j != right+1) {
+            ++index;
+            findPostOrder(left, j-1, pree, inn);
+            findPostOrder(j+1, right, pree, inn);
+            cout << inn[j];
+        }
+    } else return; //bottom of recursion
 
 }
