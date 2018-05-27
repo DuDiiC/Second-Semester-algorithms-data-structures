5b3dd09791e9f906cc08b54af460a5fbde193835
Maciej Dudek <maciejdudek96@wp.pl>
2018-05-27 12:46:22 +0200

    Change the implementation Periodic String

--- [H1] Periodic String/Periodic String.cpp
+++ [H1] Periodic String/Periodic String.cpp
@@ -1,37 +1,38 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include<iostream>
+#include<string>
+
 using namespace std;
 
-int Knuth_Morris_Pratt(string word) {
-    vector <int>  tab(100);
-    tab[0] = -1;
-    int i = 1, j = -1;
-    while(word[i]) {
-        while(j >= 0 && word[j+1] != word[i]) {
-            j = tab[j];
+int shortestPeriod(string word) {
+
+    int period = 1;
+    string comparison;
+
+    while (period <= word.size()) {
+
+        comparison = "";
+        while (comparison.size() < word.size()) {
+
+            comparison += word.substr(0, period);
         }
-        if(word[j+1] == word[i]) ++j;
-        tab[i] = j;
-        ++i;
+
+        if (comparison == word) return period;
+
+        period++;
     }
-    return j;
 }
 
 int main() {
+
+    string word;
     int cases;
+
     cin >> cases;
+    while (cases--) {
 
-    while(cases--) {
-
-        string word;
         cin >> word;
-        int length = word.size();
-        int rep = Knuth_Morris_Pratt(word);
-
-        if(length%(length-rep-1)) cout << length;
-        else cout << length-rep-1;
-
-        if(cases) cout << endl;
+        cout << shortestPeriod(word) << endl;
     }
+
+    return 0;
 }
